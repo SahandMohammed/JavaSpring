c63908d9f86f4d8288e2b23b771b84bb6e38b6fb
Sahand N. Mohammed <snqiu210234@uniq.edu.iq>
2024-06-04 10:34:17 +0300

    test3

--- src/main/java/com/uniq/rawa/RawaApplication.java
+++ src/main/java/com/uniq/rawa/RawaApplication.java
@@ -12,5 +12,6 @@ public class RawaApplication {
         System.out.println("Hello World");
         System.out.println("a test");
         System.out.println("another change");
+        System.out.println("another change");
     }
 }
