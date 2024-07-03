47f81eba945241a556a2b04b387e0b91cf275c02
Sahand N. Mohammed <snqiu210234@uniq.edu.iq>
2024-07-03 15:08:53 +0300

    Testing PHR-1

--- src/main/java/com/uniq/rawa/RawaApplication.java
+++ src/main/java/com/uniq/rawa/RawaApplication.java
@@ -40,5 +40,7 @@ public class RawaApplication {
         System.out.println("jmeter test");
         System.out.println("jmeter test");
         System.out.println("jmeter test");
+        System.out.println("jmeter test");
+        System.out.println("jmeter test");
     }
 }
